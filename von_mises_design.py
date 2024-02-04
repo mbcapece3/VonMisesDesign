7a9dcce8c82b2d73a59ec5593e60a47193344336
Matthew Capece <mbcapece3@gmail.com>
2024-02-04 13:42:36 -0500

    updates

--- von_mises_design.py
+++ von_mises_design.py
@@ -4,42 +4,58 @@ import numpy as np
 from matplotlib.widgets import Slider, Button, RadioButtons
 import pandas as pd
 
-zeta_TE = 1 + 0j  # fix trailing edge at (1,0)
-
-
 class Circle:
-    def __init__(self,center,num_pts=1000):
+    def __init__(self,center,singularities=[1+0j, -1+0j],num_pts=200):
+        self.zeta_TE = 1 + 0j  # fix trailing edge at (1,0)
         self.center = center # complex number
-        self.radius = abs(zeta_TE - self.center)
+        self.singularities = singularities # list of singularities as complex points
+        self.radius = abs(self.zeta_TE - self.center)
         self.beta = np.arcsin(np.imag(self.center)/self.radius)
-        self.num_pts = num_pts  # number of discreet points in circle
-        #print(self.beta*180/np.pi)
+        self.num_pts = num_pts  # number of discreet points in circle 
+        self.angles = np.arange(0, 2*np.pi, 2*np.pi / self.num_pts)
+
+        circle_x_vals = self.radius * np.cos(self.angles) + np.real(self.center)
+        circle_y_vals = self.radius * np.sin(self.angles) + np.imag(self.center)
+
+        circle_data = {
+            "angle": self.angles,
+            "x_pts": circle_x_vals,
+            "y_pts": circle_y_vals,
+        }
+
+        self.circle_df = pd.DataFrame(circle_data)
+        
         
     def setCenter(self,new_center):
         self.center = new_center # complex number
-        self.radius = abs(zeta_TE - self.center)
+        self.radius = abs(self.zeta_TE - self.center)
         self.beta = np.arcsin(np.imag(self.center)/self.radius)
 
+        circle_x_vals = self.radius * np.cos(self.angles) + np.real(self.center)
+        circle_y_vals = self.radius * np.sin(self.angles) + np.imag(self.center)
+
+        self.circle_df["x_pts"] = circle_x_vals
+        self.circle_df["y_pts"] = circle_y_vals
+
+
+    def setSingularities(self, newSingularities):
+        self.singularities = newSingularities # list of singularities as complex points
+
     def plotCircle(self):
-        angles = np.arange(0, 2*np.pi, 2*np.pi / self.num_pts)
-        circle_x_vals = self.radius * np.cos(angles) + np.real(self.center)
-        circle_y_vals = self.radius * np.sin(angles) + np.imag(self.center)
-        circle_data = {
-            "angle": angles,
-            "x_pt": circle_x_vals,
-            "y_pt": circle_y_vals
-        }
-        circle_df = pd.DataFrame(circle_data)
-        
         fig, ax = plt.subplots()
-        ax.plot(circle_df.x_pt, circle_df.y_pt)
+        ax.plot(self.circle_df.x_pts, self.circle_df.y_pts)
         ax.scatter(np.real(self.center), np.imag(self.center))
+        ax.scatter(np.real(self.singularities),np.imag(self.singularities))
         ax.set_aspect('equal')
         ax.set_xlim(-2,2)
         ax.set_ylim(-2,2)
         ax.grid()
+        ax.vlines(0,-2,2,color='black')
+        ax.hlines(0,-2,2,color='black')
 
-        plt.show()
+#class ConformalMap:
+
+
         
 
 # class Airfoil:
@@ -49,6 +65,14 @@ class Circle:
 
 #     def drawAirfoil():
 #         print('test')
-        
-test = Circle(-.072+.29j)
+
+singularities = [1+0j, -1+0j]
+singularities2 = [1+0j, -1+0j, 0+1j, 0-1j]
+
+test = Circle(-.072+.29j,singularities)
 test.plotCircle()
+test.setCenter(-.15+.5j)
+test.plotCircle()
+test.setSingularities(singularities2)
+test.plotCircle()
+plt.show()
