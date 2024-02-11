e75021999a5dcbbde6a6cb4e17961c151f66a0fa
Matthew Capece <mbcapece3@gmail.com>
2024-02-10 23:33:06 -0500

    unlocked the secret formula

--- von_mises_design.py
+++ von_mises_design.py
@@ -3,9 +3,11 @@ import matplotlib.patches as patches
 import numpy as np
 from matplotlib.widgets import Slider, Button, RadioButtons
 import pandas as pd
+import itertools
 
 class VonMises:
-
+    
+            
     def __init__(self,center,singularities=[1+0j, -1+0j, 0, 0],num_pts=200):
         self.zeta_TE = 1 + 0j  # fix trailing edge at (1,0)
         self.center = center # complex number
@@ -26,10 +28,29 @@ class VonMises:
         s3 = self.singularities[2]
         s4 = self.singularities[3]
 
-        self.C1 = s1**2 + s2*(s3+s4) - s3*s4
-        self.C2 = 1/2*(s1*(s2*(s3*s4) + s3*s4) + s2*s3*s4)
-        self.C3 = -1/3*(s1*s2*s3*s4)
+        self.C1 = self.compute_vonmises_coeff(1)
+        self.C2 = self.compute_vonmises_coeff(2)
+        self.C3 = self.compute_vonmises_coeff(3)
+
+        # self.C1 = s1**2 - s2*(s3+s4) - s3*s4
+        # self.C2 = 1/2*(s1*(s2*(s3+s4) + s3*s4) + s2*s3*s4)
+        # self.C3 = -1/3*(s1*s2*s3*s4)
+
+    def compute_vonmises_coeff(self, coeff_num):
+            num_terms = coeff_num + 1
+            k = len(self.singularities)
+            sum = 0
         
+            for indices in itertools.combinations(range(k), num_terms):
+                product = 1
+                for idx in indices:
+                    product *= self.singularities[idx]
+                sum += product
+
+            if (coeff_num % 2) == 0:
+                return 1 / coeff_num * sum
+            else:
+                return -1 / coeff_num * sum 
 
     def setCenter(self,new_center):
         self.center = new_center # complex number
@@ -47,12 +68,24 @@ class VonMises:
         s3 = self.singularities[2]
         s4 = self.singularities[3]
 
-        self.C1 = s1**2 + s2*(s3+s4) - s3*s4
-        self.C2 = 1/2*(s1*(s2*(s3*s4) + s3*s4) + s2*s3*s4)
-        self.C3 = -1/3*(s1*s2*s3*s4)
+        self.C1 = VonMises.compute_vonmises_coeff(test.singularities,1)
+        self.C2 = VonMises.compute_vonmises_coeff(test.singularities,2)
+        self.C3 = VonMises.compute_vonmises_coeff(test.singularities,3)
+        # self.C1 = s1**2 - s2*(s3+s4) - s3*s4
+        # self.C2 = 1/2*(s1*(s2*(s3+s4) + s3*s4) + s2*s3*s4)
+        # self.C3 = -1/3*(s1*s2*s3*s4)
 
     def conformalMap(self, v_inf, alpha_rad):
         self.circle_df["airfoil_pts"] = self.circle_df.circle_pts + self.C1/self.circle_df.circle_pts + self.C2/(self.circle_df.circle_pts**2) + self.C3/(self.circle_df.circle_pts**3)
+
+        # SCALE AIRFOIL PTS TO X/C
+        #####################################################################
+        #####################################################################
+        #####################################################################
+        #####################################################################
+        #####################################################################
+        #####################################################################
+        
         self.circle_df["circle_vels"] = 2*v_inf*(np.sin(alpha_rad + self.beta) - np.sin(alpha_rad - self.circle_df.angles))
         self.circle_df["map_func_derivatives"] = 1 - self.C1/(self.circle_df.circle_pts**2) - self.C2/(self.circle_df.circle_pts**3) - self.C3/(self.circle_df.circle_pts**4)  # first derivative of airfoil pts wrt circle pts
         self.circle_df["airfoil_vels"] = abs(self.circle_df.circle_vels) / abs(self.circle_df.map_func_derivatives)
@@ -100,14 +133,19 @@ class VonMises:
 
         
 singularities = [1+0j, -1+0j, 0, 0]
-singularities2 = [1+0j, -1+0j, 0+1j, 0-1j]
+singularities2 = [1+0j, -1+0j, .37-.471j, -.37+.471j]
  
-test = VonMises(-.072+.29j,singularities)
+test = VonMises(-.072+.29j,singularities2)
 test.conformalMap(1,0)
 #print(test.circle_df)
 test.plotCircle()
-test.setCenter(-.15+.5j)
-test.plotCircle()
-test.setSingularities(singularities2)
-test.plotCircle()
+#test.setCenter(-.15+.5j)
+#test.plotCircle()
+#test.setSingularities(singularities2)
+#test.conformalMap(1,0)
+#test.plotCircle()
+
 plt.show()
+
+
+
